dff606a761fc7e4397b58a87d657cc1fb04cb9ff
Sumit <Sumit@DESKTOP-H7AK8FK>
2024-05-02 13:43:05 +0530

    Updating the Program

--- Coding2024/src/ReadAndReverse.java
+++ Coding2024/src/ReadAndReverse.java
@@ -10,6 +10,8 @@ public class ReadAndReverse {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome HSBC, Thank You for Considering my Candidature for the Role");
+		System.out.println("Reading input file input.txt in the project");
+		System.out.println("#####################");
 		try(PrintWriter out = 
 		        new PrintWriter(Files.newBufferedWriter(Paths.get("output.txt")))) {
 		    Files.lines(Paths.get("input.txt"))
@@ -17,8 +19,12 @@ public class ReadAndReverse {
 		          .collect(Collectors.toCollection(LinkedList::new))
 		         .iterator()
 		         .forEachRemaining(out::println);
+		    
+			System.out.println("Writing Reversed content of the input file to output.txt file in the project");
+			System.out.println("#####################");
 		}catch (Exception e) {
 			System.out.println(" There was an error while reading or Writing File, PLease check exception for details"+e.getMessage());
 		}
+		System.out.println("##########THANK YOU ###########");
 	}
 }
