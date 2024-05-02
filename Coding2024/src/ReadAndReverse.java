ca7fa7d2480ee130599a4c1605df61cf9e233132
Sumit <Sumit@DESKTOP-H7AK8FK>
2024-05-02 13:00:04 +0530

    Commiting all files for First time

---
+++ Coding2024/src/ReadAndReverse.java
@@ -0,0 +1,29 @@
+
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ReadAndReverse {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome HSBC, Thank You for Considering my Candidature for the Role");
+		
+		
+		try(PrintWriter out = 
+		        new PrintWriter(Files.newBufferedWriter(Paths.get("output.txt")))) {
+		    Files.lines(Paths.get("input.txt"))
+		    .flatMap(line -> Stream.of(new StringBuilder(line.toString()).reverse()))
+		          .collect(Collectors.toCollection(LinkedList::new))
+		         .iterator()
+		         .forEachRemaining(out::println);
+		}catch (Exception e) {
+
+			System.out.println(" There was an eroor while reading or Writing File, PLease check exception for details"+e.getMessage());
+		}
+		
+		
+	}
+}
